bea30a1d3b0e419c3dc8832b4d7268b58280c853
anas-er25 <anaserrakibi@gmail.com>
2024-08-10 00:06:14 +0100

    Arithmetic operators

---
+++ Arithmetic_Operators.java
@@ -0,0 +1,19 @@
+public class Arithmetic_Operators {
+	public static void main(String[] args) {
+		// double num1 = 10.50;
+		// double num2 = 0.5;
+
+		// double sum = num1 + num2;
+		// double difference = num1 - num2;
+		// double product = num1 * num2;
+		// double quotient = num1 / num2;
+		// double module = num1 % num2;
+
+		// System.out.println("The sum of the two numbers is: " + sum);
+		// System.out.println("The difference of the two numbers is: " + difference);
+		// System.out.println("The product of the two numbers is: " + product);
+		// System.out.println("The quotient of the two numbers is: " + quotient);
+        // System.out.println("The module of the two numbers is: " + module);
+        System.out.println(15 % 4);
+	}
+}
