f8df5b816380be9b9ad3f5fa4ed866474182f697
anas-er25 <anaserrakibi@gmail.com>
2024-08-11 11:51:15 +0100

    boolean operators

---
+++ Boolean_Operators.java
@@ -0,0 +1,15 @@
+public class Boolean_Operators {
+    public static void main(String[] args){
+        // int a = 5;                                       //boolean d = true;
+        // int b = 2;                                       //boolean e = false;
+        //boolean c = true;                                 //boolean f = false;
+       
+        // System.out.println(a == b); //false               //System.out.println(c && d); //true
+        // System.out.println(a != b); //true                //System.out.println(c && f); //false
+        // System.out.println(a < b); //false                //System.out.println(c && d && e); //false
+        // System.out.println(a <= b); //false               //System.out.println(c || d || e); //true
+        // System.out.println(a > b); //true                 //System.out.println(c || d); //true
+        // System.out.println(a >= b); //true                //System.out.println(e || f); //false
+    
+    }
+}
